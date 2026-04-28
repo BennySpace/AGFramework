74b79640f4ed055c8f36ff9be313e25ca92e9a1f
Daniil Vlasov <356550@edu.itmo.ru>
2026-04-28 22:37:58 +0300

    feat(shaders): add diffuse texture sampling to Phong shader

--- AGFramework/shaders/Phong.hlsl
+++ AGFramework/shaders/Phong.hlsl
@@ -12,10 +12,14 @@ cbuffer ObjectConstants : register(b0)
     float4 gSpecularAlbedo;
 }
 
+Texture2D gDiffuseMap : register(t0);
+SamplerState gsamLinearWrap : register(s0);
+
 struct VertexIn
 {
     float3 PosL : POSITION;
     float3 NormalL : NORMAL;
+    float2 TexC : TEXCOORD;
 };
 
 struct VertexOut
@@ -23,6 +27,7 @@ struct VertexOut
     float4 PosH : SV_POSITION;
     float3 PosW : POSITION;
     float3 NormalW : NORMAL;
+    float2 TexC : TEXCOORD;
 };
 
 VertexOut VS(VertexIn vin)
@@ -33,12 +38,15 @@ VertexOut VS(VertexIn vin)
     vout.PosW = posW.xyz;
     vout.NormalW = mul(vin.NormalL, (float3x3)gWorldInvTranspose);
     vout.PosH = mul(float4(vin.PosL, 1.0f), gWorldViewProj);
+    vout.TexC = vin.TexC;
 
     return vout;
 }
 
 float4 PS(VertexOut pin) : SV_Target
 {
+    float4 texColor = gDiffuseMap.Sample(gsamLinearWrap, pin.TexC);
+    float3 surfaceAlbedo = texColor.rgb * gDiffuseAlbedo.rgb;
     float3 normalW = normalize(pin.NormalW);
     float3 toEye = normalize(gEyePosW - pin.PosW);
     float3 lightDir = normalize(-gLightDir.xyz);
@@ -48,9 +56,9 @@ float4 PS(VertexOut pin) : SV_Target
     float specularPower = gSpecularAlbedo.w;
     float specularFactor = pow(saturate(dot(normalW, halfVector)), specularPower);
 
-    float3 ambient = gAmbientLight.rgb * gDiffuseAlbedo.rgb;
-    float3 diffuse = ndotl * gLightColor.rgb * gDiffuseAlbedo.rgb;
+    float3 ambient = gAmbientLight.rgb * surfaceAlbedo;
+    float3 diffuse = ndotl * gLightColor.rgb * surfaceAlbedo;
     float3 specular = specularFactor * gLightColor.rgb * gSpecularAlbedo.rgb;
 
-    return float4(ambient + diffuse + specular, gDiffuseAlbedo.a);
+    return float4(ambient + diffuse + specular, texColor.a * gDiffuseAlbedo.a);
 }
