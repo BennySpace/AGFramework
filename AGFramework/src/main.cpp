ac8af1bff024ef0421eb1293da1a0bfb45f22aa6
Daniil Vlasov <356550@edu.itmo.ru>
2026-04-29 20:27:55 +0300

    feat(main): handle DirectX and unknown exceptions in WinMain

--- AGFramework/src/main.cpp
+++ AGFramework/src/main.cpp
@@ -1,4 +1,5 @@
 #include "App/AGFramework.h"
+#include "Graphics/dx12/d3dUtil.h"
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, int nShowCmd) {
     try {
@@ -6,8 +7,16 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, 
         application.Initialize();
         application.Run();
     }
+    catch (const DxException& e) {
+        MessageBoxW(nullptr, e.ToString().c_str(), L"DirectX Error", MB_ICONERROR);
+        return 1;
+    }
     catch (const std::exception& e) {
         MessageBoxA(nullptr, e.what(), "Engine Error", MB_ICONERROR);
+        return 1;
+    }
+    catch (...) {
+        MessageBoxA(nullptr, "Unknown fatal error.", "Engine Error", MB_ICONERROR);
         return 1;
     }
 
